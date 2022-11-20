82cab1939272a33a89a498227e1fe793bfa12b29
saltire sable <saltiresable@gmail.com>
2022-11-20 16:33:49 -0500

    Update packages

--- client/index.tsx
+++ client/index.tsx
@@ -1,6 +1,7 @@
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
 
-render(<App />, document.querySelector('#root'));
+createRoot(document.querySelector('#root')!)
+  .render(<App />);
